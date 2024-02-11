016a35113398414b41f3d7cdd24c9734caa162b4
pin150802 <a0842013735@gmail.com>
2024-02-11 18:59:45 +0700

    projectfont

---
+++ src/layout/Header.jsx
@@ -0,0 +1,46 @@
+import {Link, useNavigate} from 'react-router-dom'
+import useAuth from '../hooks/useAuth';
+
+const guestNav = [
+  { to : '/', text: 'Login' },
+  { to : '/register', text: 'Register' },
+]
+
+const userNav = [
+  { to : '/', text: 'Home' },
+{ to : '/new', text: 'บัญชีของฉัน'},
+]
+
+export default function Header() {
+  const {user, logout} = useAuth() 
+  const finalNav = user?.id ? userNav : guestNav
+
+  const navigate = useNavigate()
+
+  const hdlLogout = () => {
+    logout()
+    navigate('/')
+  }
+
+  return (
+    <div className="navbar bg-blue-800">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl text-white">Bookmarket, {user?.id ? user.username : 'Guest'}</a>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          { finalNav.map( el => (
+            <li key={el.to} >
+              <Link to={el.to}>{el.text}</Link>
+            </li>
+          ))}
+          { user?.id && (
+            <li>
+              <Link to='#' onClick={hdlLogout}>Logout</Link>
+            </li>
+          ) }
+        </ul>
+      </div>
+    </div>
+  );
+}
